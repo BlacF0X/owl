cdac7d1288cf6dfed69c07d4d4a1b7a52655ee77
martin <martin.Stocq@gmail.com>
2025-10-07 11:46:06 +0200

    page d'accueil #15

--- owl-web-app/src/App.tsx
+++ owl-web-app/src/App.tsx
@@ -1,26 +1,10 @@
+// App.tsx
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import HomePage from './HomePage';
+import './App.css'; // Assurez-vous d'avoir un fichier CSS pour les styles
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+  return <HomePage />;
+};
 
 export default App;
