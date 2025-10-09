a32b2705fa05357febbf872ebb7aacbb8d5d99c5
Liam Gérard <l.gerard@students.ephec.be>
2025-10-09 18:21:00 +0200

    Refonte de la page d'accueil suivant la maquette->prototype de la page d'accueil terminé #15

--- owl-web-app/src/components/Stats.js
+++ owl-web-app/src/components/Stats.js
@@ -5,16 +5,15 @@ import '../styles/Stats.css'
 function Stats() {
   return (
     <section className="stats" id="stats">
-      <h2>Quelques chiffres</h2>
+      <h2>À propos de nous</h2>
       <div className="stats-grid">
         <div className="stat">
-          <h3>Concentration en CO2</h3>
-          <p>950PPM</p>
-          <img id='imgCO' src='https://actugeologique.fr/wp-content/uploads/2023/04/Concentration-en-CO2-b-1024x612.jpg'></img>
+          <h3>120 000</h3>
+          <p>Utilisateurs dans le monde</p>
         </div>
         <div className="stat">
-          <h3>98%</h3>
-          <p>Précision moyenne</p>
+          <h3>⭐⭐⭐⭐/5</h3>
+          <p>Selon nos clients</p>
         </div>
         <div className="stat">
           <h3>24/7</h3>
