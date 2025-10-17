8e466b71ca4f6c2e8dfe13d897708cc7d7d011a9
Clément <c.vier@students.ephec.be>
2025-10-17 12:29:26 +0200

    migration vers create-next-app pour résoudre des conflits avec eslint et create-react-app

---
+++ owl-next-app/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import "@/styles/App.css";
+import "@/styles/Navbar.css";
+import "@/styles/Footer.css";
+import "@/styles/Home.css";
+
+export const metadata: Metadata = {
+  title: "Project OwL",
+  description: "Surveillez l'environnement en temps réel",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fr">
+      <body>
+        <Navbar />
+        <main>{children}</main> {/* Le contenu de la page sera inséré ici */}
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ owl-next-app/app/page.tsx
@@ -0,0 +1,11 @@
+import Home from "@/components/Home";
+import HomePage from "@/pages/HomePage";
+
+export default function Page() {
+  return (
+    <div>
+      <Home />
+      <HomePage />
+    </div>
+  );
+}
