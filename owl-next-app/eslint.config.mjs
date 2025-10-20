1ac75a21e58a515e1406dc918868f38c107bf9b7
Clément <c.vier@students.ephec.be>
2025-10-20 09:04:45 +0200

    run prettier

--- owl-next-app/eslint.config.mjs
+++ owl-next-app/eslint.config.mjs
@@ -15,10 +15,10 @@ const eslintConfig = [
     ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
   },
   {
-  rules: {
-    "react/no-unescaped-entities": "off"
-    }
-  }
+    rules: {
+      'react/no-unescaped-entities': 'off',
+    },
+  },
 ];
 
 export default eslintConfig;
