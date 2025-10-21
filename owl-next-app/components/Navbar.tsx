55ce13b56ad1de955c93dbea3bef20808269f700
Arno <a.starkel@students.ephec.be>
2025-10-21 12:23:18 +0200

    Ajout d'un bouton 'S'inscrire' à la Navbar #58

--- owl-next-app/components/Navbar.tsx
+++ owl-next-app/components/Navbar.tsx
@@ -1,25 +1,37 @@
 import React from 'react';
 import styles from './Navbar.module.css';
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+import Link from 'next/link';
 
 const Navbar: React.FC = () => {
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>OwL.</div>
       <ul className={styles.navLinks}>
-        <li>
-          <a href="#home">Accueil</a>
+                <li>
+          <a href="/home">Accueil</a>
         </li>
         <li>
-          <a href="#mySpace">Mon espace</a>
+          <a href="/dashboard">Mon espace</a>
         </li>
         <li>
           <a href="#tips">Astuces</a>
         </li>
-        <li>
-          <a href="/connexion" className={styles.connection}>
-            Se connecter
-          </a>
-        </li>
+        <SignedIn>
+          <li>
+            <UserButton afterSignOutUrl="/" />
+          </li>
+        </SignedIn>
+        <SignedOut>
+          <li>
+            <Link href="/inscription">S'inscrire</Link>
+          </li>
+          <li>
+            <Link href="/connexion" className={styles.connection}>
+              Se connecter
+            </Link>
+          </li>
+        </SignedOut>
       </ul>
     </nav>
   );
