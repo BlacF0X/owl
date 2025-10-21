af726fd368fcd67aed0b8a7286f4fcecb6141bfa
Arno <a.starkel@students.ephec.be>
2025-10-21 11:38:34 +0200

    Modification du fichier middleware.ts #54

--- owl-next-app/app/layout.tsx
+++ owl-next-app/app/layout.tsx
@@ -3,6 +3,7 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer'
 import { ClerkProvider } from '@clerk/nextjs';
 import { frFR } from "@clerk/localizations";
+import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Project OwL',
