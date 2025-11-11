643965672dcf3a8d53031567709ce7cb4ec81341
Clément <c.vier@students.ephec.be>
2025-11-11 23:18:21 +0100

    [200~import express from 'express';

--- owl-api/src/api/routes/test.routes.ts
+++ owl-api/src/api/routes/test.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 import { getDbConnectionTest } from '../controllers/test.controller.js';
+import express from 'express';
+
 
 const router = Router();
+
+router.use(express.json());
 
 // Définit la route GET /db-test et lui attache le contrôleur
 router.get('/db-test', getDbConnectionTest);
