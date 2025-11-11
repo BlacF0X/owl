578b1ae125527d46b3b5f1ed890b7fffcd72233a
Clément <c.vier@students.ephec.be>
2025-11-11 22:45:40 +0100

    webhook clerk #114

--- owl-api/src/api/routes/index.ts
+++ owl-api/src/api/routes/index.ts
@@ -1,14 +1,12 @@
 import { Router } from 'express';
 import testRouter from './test.routes.js';
+import clerkWebhookRouter from './clerk.routes.js';
 
 const apiRouter = Router();
 
 // On "monte" le routeur de test. Toutes ses routes seront accessibles.
 apiRouter.use(testRouter);
 
-// --- PRÉPARATION POUR LE FUTUR WEBHOOK CLERK ---
-// Quand vous serez prêt, vous créerez clerk.routes.ts et décommenterez ces lignes.
-// import clerkWebhookRouter from './clerk.routes.js';
-// apiRouter.use('/webhooks/clerk', clerkWebhookRouter);
+apiRouter.use('/webhooks/clerk', clerkWebhookRouter);
 
 export default apiRouter;
