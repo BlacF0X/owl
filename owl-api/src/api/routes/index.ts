b5c621b44a35dc3e6acb494f9dfd0117a7f3499a
Martin Stocq <martin.stocq@gmail.com>
2025-11-25 22:52:20 +0100

    US50 Changement page.tsx

--- owl-api/src/api/routes/index.ts
+++ owl-api/src/api/routes/index.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express';
 import testRouter from './test.routes.js';
 import clerkWebhookRouter from './clerk.routes.js';
 import sensorRouter from './sensor.routes.js';
+import co2Router from './co2.routes.js'
 const apiRouter = Router();
 
 
@@ -12,6 +13,6 @@ apiRouter.use('/webhooks/clerk', clerkWebhookRouter);
 apiRouter.use(express.json());
 apiRouter.use(testRouter);
 apiRouter.use('/sensors', sensorRouter);
-
+apiRouter.use('/co2', co2Router); 
 
 export default apiRouter;
