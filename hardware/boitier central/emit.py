a2b324a8e0635a5903be703e96b86c02d483b1cf
Corentin Mertens <c.mertens@students.ephec.be>
2025-12-05 14:10:14 +0100

    ajout de code pour l'emission et tout ce qui est relatif au json plus rearrangement du git

--- hardware/boitier central/emit.py
+++ hardware/boitier central/emit.py
@@ -1,12 +1,9 @@
 import network
 import time
 from picozero import pico_led
-ssid = 'ssid'
-password = 'password'
 
 
-
-def emit(ssid,password):
+def emit(ssid, password):
     ap = network.WLAN(network.AP_IF)
     ap.config(essid=ssid, password=password)
     ap.active(True)
@@ -14,12 +11,15 @@ def emit(ssid,password):
         time.sleep(1)
     pico_led.on()
     print('Point d\'accès actif. IP:', ap.ifconfig()[0])
+    while not ap.isconnected():
+        print('waiting for device')
+        time.sleep(1)
     return ap
-
 
 
 def un_emit(ap):
     ap.active(False)
     ap.deinit()
     ap = None
+
 
